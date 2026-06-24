6b58ea2aa03fdb1c296cb4c0ffdd110583efade5
rocksoncodes <michaelrockszn@gmail.com>
2026-06-24 01:36:05 +0000

    refactor(dotenv): centralize env variable injection
    - add new method to extract error messages for the controller handler for more context

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,13 @@
-import dotenv from "dotenv";
 import express, { type Express } from "express";
 import morgan from "morgan";
 import logger from "./shared/server.logger";
 import { apiRouter } from "./modules/routes.registry";
-
-dotenv.config();
+import { config } from "./utils/env.config";
 
 const server: Express = express();
-const port: number = Number(process.env.PORT) || 3000;
+const port: number = Number(config.port) || 3000;
+const environment: string = config.environment ?? "dev";
+
 server.use(
   morgan("combined", {
     stream: { write: (message) => logger.info(message.trim()) },
@@ -18,7 +18,6 @@ server.use(express.json());
 server.use("/api", apiRouter);
 
 server.listen(port, (): void => {
-  logger.info(`Server running on ${port}`, {
-    serverEnvironment: process.env.ENVIRONMENT ?? "dev",
-  });
+  logger.info(`Server running on ${port}`);
+  logger.info(`Server environment: ${environment}`);
 });
