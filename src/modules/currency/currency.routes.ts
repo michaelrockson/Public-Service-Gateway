1142efad3a5d2c3e80b4ae507ac8077f2f5e48ec
Michael Rockson <michaelrockszn@gmail.com>
2026-07-06 23:39:35 +0000

    feat: add currency routes

---
+++ src/modules/currency/currency.routes.ts
@@ -0,0 +1,17 @@
+import { Request, Response, Router } from "express";
+import { CurrencyController } from "./currency.controller.js";
+
+export function createCurrencyRouter(
+  currencyController: CurrencyController,
+): Router {
+  const currencyRouter = Router();
+
+  currencyRouter.get(
+    "/currency",
+    async (req: Request, res: Response): Promise<void> => {
+      await currencyController.handleCurrencyRateRequest(req, res);
+    },
+  );
+
+  return currencyRouter;
+}
