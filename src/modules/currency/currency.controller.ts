30f890a94632e041cb22686dda8192fdbd6cb81c
Michael Rockson <michaelrockszn@gmail.com>
2026-07-06 23:30:41 +0000

    feat: add currency controller

---
+++ src/modules/currency/currency.controller.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+import { CurrencyService } from "./currency.service.js";
+import { ControllerResponseHandler } from "../../shared/http.controller.js";
+
+export class CurrencyController {
+  private readonly httpClient: CurrencyService;
+  private readonly responseHandler: ControllerResponseHandler;
+
+  constructor(currencyService: CurrencyService) {
+    this.httpClient = currencyService;
+    this.responseHandler = new ControllerResponseHandler();
+  }
+
+  async handleCurrencyRateRequest(req: Request, res: Response) {
+    return this.responseHandler.handleRequest(
+      req,
+      res,
+      (params) => this.httpClient.getCurrencyRates(params),
+      "currencyRates",
+      ["currency"],
+    );
+  }
+}
