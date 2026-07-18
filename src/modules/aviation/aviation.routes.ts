5cdfe3486d5cc8fa14a5387e2f923e87519a5900
Michael Rockson <michaelrockszn@gmail.com>
2026-07-18 00:04:09 +0000

    feat: register aviation router and updated URIs

--- src/modules/aviation/aviation.routes.ts
+++ src/modules/aviation/aviation.routes.ts
@@ -7,77 +7,77 @@ export function provideAviationRouter(
   const aviationRouter = Router();
 
   aviationRouter.get(
-    "flights",
+    "/flights",
     async (req: Request, res: Response): Promise<void> => {
       await aviationController.handleGetFlightsRequest(req, res);
     },
   );
 
   aviationRouter.get(
-    "flights/pasts",
+    "/flights/pasts",
     async (req: Request, res: Response): Promise<void> => {
       await aviationController.handleGetPastFlightsRequest(req, res);
     },
   );
 
   aviationRouter.get(
-    "flights/schedules",
+    "/flights/schedules",
     async (req: Request, res: Response): Promise<void> => {
       await aviationController.handleGetFlightScheduleRequest(req, res);
     },
   );
 
   aviationRouter.get(
-    "flights/futures",
+    "/flights/futures",
     async (req: Request, res: Response): Promise<void> => {
       await aviationController.handleGetFutureFlightsRequest(req, res);
     },
   );
 
   aviationRouter.get(
-    "airports",
+    "/airports",
     async (req: Request, res: Response): Promise<void> => {
       await aviationController.handleGetAirportsRequest(req, res);
     },
   );
 
   aviationRouter.get(
-    "airlines",
+    "/airlines",
     async (req: Request, res: Response): Promise<void> => {
       await aviationController.handleGetAirlinesRequest(req, res);
     },
   );
 
   aviationRouter.get(
-    "aircraft/types",
+    "/aircraft/types",
     async (req: Request, res: Response): Promise<void> => {
       await aviationController.handleAirCraftTypesRequest(req, res);
     },
   );
 
   aviationRouter.get(
-    "aircraft/registration",
+    "/aircraft/registration",
     async (req: Request, res: Response): Promise<void> => {
       await aviationController.handleGetAirCraftRegistrationRequest(req, res);
     },
   );
 
   aviationRouter.get(
-    "flights/cities",
+    "/flights/cities",
     async (req: Request, res: Response): Promise<void> => {
       await aviationController.handleGetCitiesRequest(req, res);
     },
   );
 
   aviationRouter.get(
-    "flights/countries",
+    "/flights/countries",
     async (req: Request, res: Response): Promise<void> => {
       await aviationController.handleGetCountriesRequest(req, res);
     },
   );
 
   aviationRouter.get(
-    "flights/taxes",
+    "/flights/taxes",
     async (req: Request, res: Response): Promise<void> => {
       await aviationController.handleGetTaxesRequest(req, res);
     },
